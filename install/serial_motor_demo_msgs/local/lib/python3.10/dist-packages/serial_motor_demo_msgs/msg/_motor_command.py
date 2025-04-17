b6b3c1fbe8de4ec315a8276f1e3ed3e08531a66c
simon <simoman00@gmail.com>
2025-04-17 13:44:34 +0200

    robot som funkar

--- install/serial_motor_demo_msgs/local/lib/python3.10/dist-packages/serial_motor_demo_msgs/msg/_motor_command.py
+++
@@ -1,1 +0,0 @@
-/home/user/dev_ws/build/serial_motor_demo_msgs/rosidl_generator_py/serial_motor_demo_msgs/msg/_motor_command.py
--- install/serial_motor_demo_msgs/share/serial_motor_demo_msgs/cmake/ament_cmake_export_dependencies-extras.cmake
+++
@@ -1,1 +0,0 @@
-/home/user/dev_ws/build/serial_motor_demo_msgs/ament_cmake_export_dependencies/ament_cmake_export_dependencies-extras.cmake
