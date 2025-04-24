871b8418e2dd671c0d6273425cda3ebb74991425
Trenaddictus <Trenaddictus@protonmail.com>
2025-04-24 16:43:18 +0200

    removed the build install and log folders from building the directory

--- build/serial_motor_control/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/user/vacuum_robot_ws/install/serial_motor_msgs/share/serial_motor_msgs/package.sh"
--- build/serial_motor_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/user/vacuum_robot_ws/install/serial_motor_control'
